926f9953f39588b3844ff39f3802f3c8b47061ea
Scott Simpson <Scott@simply-simpy.com>
2021-01-08 08:39:20 -0800

    mat drop down sorting working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,19 @@ import { Component } from '@angular/core';
         </button>
       </div>
     </div>
+    <div class="devices-controls">
+      <mat-form-field class="devices-sort" appearance="fill">
+        <mat-label>Sort by</mat-label>
+        <mat-select disableRipple>
+          <mat-option *ngFor="let column of columns" (click)="sort(column.prop)">
+            {{ column.prop }}
+          </mat-option>
+          <ng-template #nameTemplate let-rowIndex="rowIndex" let-row="row" let-value="value" ngx-datatable-cell-template>
+            <span>{{value.name}}</span>
+          </ng-template>
+        </mat-select>
+      </mat-form-field>
+    </div>
     <div>
       <ngx-datatable
         [rows]="rows"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
+
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatOptionModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [NgxDatatableModule, BrowserModule],
+  imports: [NgxDatatableModule, BrowserModule, NoopAnimationsModule, MatOptionModule, MatFormFieldModule, MatSelectModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
